41c9943253aa3c9bad1fdcb0dbe8257a3e4ee3e6
Ben Bricker <brickerbm2@vcu.edu>
2023-05-17 15:08:39 -0400

    feat: add custom domain name to cf dist

--- infra/lib/cloudfront-stack.ts
+++ infra/lib/cloudfront-stack.ts
@@ -1,20 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
+import {
+  Certificate,
+  CertificateValidation,
+} from 'aws-cdk-lib/aws-certificatemanager';
 import { Distribution, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
 import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 const assetPath = './resources/build';
+const hostedZoneDomainName = 'brickerfam.com';
+const cloudfrontRecordName = 'ben';
+const cloudfrontDomainName = `${cloudfrontRecordName}.${hostedZoneDomainName}`;
 
 export class CloudfrontStack extends cdk.Stack {
   appBucket: Bucket;
   distribution: Distribution;
   deployment: BucketDeployment;
+  certificate: Certificate;
+  aRecord: ARecord;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const importedHostedZone = HostedZone.fromLookup(this, 'hosted-zone', {
+      domainName: hostedZoneDomainName,
+    });
+
+    this.certificate = new Certificate(this, 'cloudfront-certificate', {
+      domainName: cloudfrontDomainName,
+      validation: CertificateValidation.fromDns(importedHostedZone),
+    });
 
     this.appBucket = new Bucket(this, 'app-bucket', {
       autoDeleteObjects: true,
@@ -23,6 +43,8 @@ export class CloudfrontStack extends cdk.Stack {
     });
 
     this.distribution = new Distribution(this, 'cloudfront-distribution', {
+      domainNames: [cloudfrontDomainName],
+      certificate: this.certificate,
       defaultBehavior: {
         origin: new S3Origin(this.appBucket),
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
@@ -42,6 +64,12 @@ export class CloudfrontStack extends cdk.Stack {
       destinationBucket: this.appBucket,
       distribution: this.distribution,
       distributionPaths: ['/*'],
+    });
+
+    this.aRecord = new ARecord(this, 'distribution-alias-record', {
+      zone: importedHostedZone,
+      recordName: 'ben',
+      target: RecordTarget.fromAlias(new CloudFrontTarget(this.distribution)),
     });
   }
 }
