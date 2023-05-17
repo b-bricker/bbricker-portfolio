caabb3bf60012e060cbdea706fb2c3f77a2a04c1
Ben Bricker <brickerbm2@vcu.edu>
2023-05-17 15:16:16 -0400

    chore: remove default stack

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { InfraStack } from '../lib/infra-stack';
 import { CloudfrontStack } from '../lib/cloudfront-stack';
 
 const app = new cdk.App();
