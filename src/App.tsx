368f2b2648fd5a3cb7ff1112691b91587963b0e1
Ben Bricker <brickerbm2@vcu.edu>
2023-05-18 11:52:18 -0400

    feat: apply styled-components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
 import Footer from './components/footer';
 import Header from './components/header';
-import styles from './App.module.scss';
+import { styled } from 'styled-components';
+
+const Paragraph = styled.p`
+  width: 80%;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  padding: 5% 0;
+  width: auto;
+`;
+
+const Main = styled.main`
+  display: flex;
+  overflow-y: auto;
+  flex: auto;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 0px;
+`;
 
 const App = (): JSX.Element => (
   <>
     <Header />
-    <main>
-      <h1 className={styles.contentheader}>Hi, I&apos;m Ben!</h1>
-      <p className={styles.contenttext}>
+    <Main>
+      <Title>Hi, I&apos;m Ben!</Title>
+      <Paragraph>
         I am a full stack developer. I enjoy working with TypeScript in React
         and Node.
-      </p>
-      <p className={styles.contenttext}>
+      </Paragraph>
+      <Paragraph>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus
         tristique nisl et cursus. Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Integer sed pulvinar lorem. Praesent et augue ut urna
         accumsan congue. Aenean aliquam arcu convallis mi ultricies, luctus
         ultricies metus molestie. Integer nec erat nunc. Curabitur ultricies
         accumsan metus et facilisis.
-      </p>
-    </main>
+      </Paragraph>
+    </Main>
     <Footer />
   </>
 );
